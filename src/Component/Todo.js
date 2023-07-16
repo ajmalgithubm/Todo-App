522c7649770ae4af6c135e0f46091f0da4637994
ajmalgithubm <ajmalm76774@gmail.com>
2023-07-16 16:20:45 +0530

    Text Alignment is Done

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 function Todo() {
     return (
-        <div style={{ display: 'flex', flexDirection: "column", padding: '10px', backgroundColor:'#1f6918'}}>
-            <div>
-                <h2>Task Name</h2>
-            </div>
-            <div style={{display:'flex',flexDirection:'row',justifyContent:'space-around',flexWrap:'wrap'}}>
+        <div style={{padding:'10px'}}>
+            <div style={{ display: 'flex', flexDirection: "column", padding: '10px', backgroundColor: '#1f6918',borderRadius:"10px",color:'white'}}>
                 <div>
-                    <p>Time Date</p>
+                    <h2 style={{textAlign:'center'}}>Task Name</h2>
                 </div>
-                <div>
-                    <p>status</p>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+                    <div>
+                        <p>created at Time Date</p>
+                    </div>
+                    <div>
+                        <p>status</p>
+                    </div>
                 </div>
             </div>
         </div>
