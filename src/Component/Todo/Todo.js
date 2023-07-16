010730652aaf0fd8e87ac073e23f400ca32bf33e
ajmalgithubm <ajmalm76774@gmail.com>
2023-07-16 22:59:57 +0530

    finish task page function is added

--- src/Component/Todo/Todo.js
+++ src/Component/Todo/Todo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Todo.css'
 
-function Todo({taskName, status, id, taskList, setAddTaskPage}) {
-    const finishTask =(e,id) => {
+function Todo({taskName, status, id, taskList, setAddTaskPage, pendingTask, finishTask}) {
+    const finishTaskFunction =(e,id) => {
         e.preventDefault()
         let driven = false;
         let currentList = [...taskList];
@@ -13,34 +13,63 @@ function Todo({taskName, status, id, taskList, setAddTaskPage}) {
             }
         }
         if(driven){
-            
+
             setAddTaskPage(currentList);
         }
         
     }
     return (
-        <div style={{ padding: '10px' }}>
-            <div style={{ display: 'flex', flexDirection: "column", padding: '10px', backgroundColor: '#1f6918', borderRadius: "10px", color: 'white', boxShadow: '0px 0px 10px 4px grey' }} className='bg-success'>
-                <div>
-                    <button type="button" className="close bg-lg closeButton border-0" aria-label="Close" style={{float:'right',width:'40px', height:'40px',borderRadius:'50%',backgroundColor:'white'}}>
-                        <span aria-hidden="true" className='text-danger font-weight-bold' >&times;</span>
-                    </button>
-                </div>
-                <div>
-                    <h4 style={{ textAlign: 'center',textDecoration:'crossline' }}>{taskName}</h4>
-                </div>
-                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap',marginTop:'50px',alignItems:'center' }}>
-                    <div>
-                        <p style={{ fontSize: "16px", fontWeight: '400' }}>19/09/2023</p>
+        <div>
+            {
+               (pendingTask && status==='incomplete') && (<div style={{ padding: '10px' }}>
+                    <div style={{ display: 'flex', flexDirection: "column", padding: '10px', backgroundColor: '#1f6918', borderRadius: "10px", color: 'white', boxShadow: '0px 0px 10px 4px grey' }} className='bg-success'>
+                        <div>
+                            <button type="button" className="close bg-lg closeButton border-0" aria-label="Close" style={{ float: 'right', width: '40px', height: '40px', borderRadius: '50%', backgroundColor: 'white' }}>
+                                <span aria-hidden="true" className='text-danger font-weight-bold' >&times;</span>
+                            </button>
+                        </div>
+                        <div>
+                            <h4 style={{ textAlign: 'center', textDecoration: 'crossline' }}>{taskName}</h4>
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap', marginTop: '50px', alignItems: 'center' }}>
+                            <div>
+                                <p style={{ fontSize: "16px", fontWeight: '400' }}>19/09/2023</p>
+                            </div>
+                            <div>
+                                <p style={{ fontSize: "16px", fontWeight: '400' }}>{status} {id}</p>
+                            </div>
+                            <div>
+                                <button className='btn btn-info btn-sm' onClick={(e) => finishTaskFunction(e, id)}>Finish</button>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <p style={{fontSize:"16px",fontWeight:'400'}}>{status} {id}</p>
+                </div>)
+            }
+            {
+                (finishTask && status === 'completed') && (<div style={{ padding: '10px' }}>
+                    <div style={{ display: 'flex', flexDirection: "column", padding: '10px', backgroundColor: '#1f6918', borderRadius: "10px", color: 'white', boxShadow: '0px 0px 10px 4px grey' }} className='bg-success'>
+                        <div>
+                            <button type="button" className="close bg-lg closeButton border-0" aria-label="Close" style={{ float: 'right', width: '40px', height: '40px', borderRadius: '50%', backgroundColor: 'white' }}>
+                                <span aria-hidden="true" className='text-danger font-weight-bold' >&times;</span>
+                            </button>
+                        </div>
+                        <div>
+                            <h4 style={{ textAlign: 'center', textDecoration: 'crossline' }}>{taskName}</h4>
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap', marginTop: '50px', alignItems: 'center' }}>
+                            <div>
+                                <p style={{ fontSize: "16px", fontWeight: '400' }}>19/09/2023</p>
+                            </div>
+                            <div>
+                                <p style={{ fontSize: "16px", fontWeight: '400' }}>{status} {id}</p>
+                            </div>
+                            <div>
+                                <button className='btn btn-danger btn-sm' onClick={(e) => finishTaskFunction(e, id)}>Finished</button>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <button className='btn btn-danger btn-sm'  onClick={(e) => finishTask(e,id)}>Finish</button>
-                    </div>
-                </div>
-            </div>
+                </div>)
+            }
         </div>
     )
 }
--- src/Component/Todos/Todos.js
+++ src/Component/Todos/Todos.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Todo from '../Todo/Todo'
-function Todos({taskList, setAddTaskPage}) {
+function Todos({taskList, setAddTaskPage, pendingTask, finishTask}) {
   
   return (
     <div style={{padding:'10px'}}>
       {
       taskList.map((task,index) => {
         return(
-          <Todo {...task} key={index} taskList = {taskList} setAddTaskPage ={setAddTaskPage}/>
+          <Todo {...task} key={index} taskList = {taskList} setAddTaskPage ={setAddTaskPage} pendingTask={pendingTask} finishTask={finishTask}/>
         )
       })
     }
